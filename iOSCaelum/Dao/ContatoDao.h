88ddd201860094b284d7019536f0aa1fbc1dd98a
ios4729 <ios4729@Rodrigo-Mac-mini-de-Caelum.local>
2015-07-09 21:44:37 -0300

    Exercício 17.6

--- iOSCaelum/Dao/ContatoDao.h
+++ iOSCaelum/Dao/ContatoDao.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 #import "Contato.h"
 
 @interface ContatoDao : NSObject
@@ -18,5 +19,12 @@
 - (Contato *) buscaContatoDaPosicao:(NSInteger)posicao;
 - (void) removeContatoDaPosicao:(NSInteger)posicao;
 - (NSInteger)buscaPosicaoDoContato:(Contato *)contato;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
